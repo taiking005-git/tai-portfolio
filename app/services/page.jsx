ec660dc8c9d99ca5ab8b01cec7978602eabea37b
Kolawole Taiwo <taiking005.dev@gmail.com>
2023-09-11 22:14:05 +0100

    services

---
+++ app/services/page.jsx
@@ -0,0 +1,31 @@
+'use client'
+
+import Circles from '../../components/Circles';
+import Bulb from '../../components/Bulb';
+
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
+import ServiceSlider from '../../components/ServicesSlider';
+
+const Services = () => {
+  return (
+    <div className='h-full bg-primary/30 py-36 flex items-center'>
+      <Circles />
+      <div className='container mx-auto'>
+        <div className='flex flex-col xl:flex-row gap-x-8'>
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+            <h2 className='h2 xl:mt-8'>My services<span className='text-accent'>.</span> </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <ServiceSlider />
+        </div>
+      </div>
+      < Bulb />
+    </div>
+
+      )
+}
+
+export default Services
